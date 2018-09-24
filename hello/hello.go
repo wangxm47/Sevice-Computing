dcb1288f8b21f3824b10cc48dd59df033e62d2eb
wangxm47 <1099422647@qq.com>
2018-09-25 01:21:46 +0800

    第一次build

---
+++ hello/hello.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/github-user/say_hello"
+)
+
+func main() {
+	say_hello.Say_hello("wzy")
+}
---
+++ say_hello/say_hello.go
@@ -0,0 +1,8 @@
+package say_hello
+
+import "fmt"
+
+func Say_hello(name string) {
+	str := name + " welcome to Golang World!!!"
+	fmt.Printf(str + "\n")
+}
